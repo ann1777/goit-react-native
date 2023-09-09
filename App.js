55809e9aff96510d288118176d55b2d21fb696c2
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-09-09 12:13:21 +0300

    Added more Roboto fonts

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { useFonts } from "expo-font";
-import React, { useState } from "react";
-import LoginScreen from "./Screen/LoginScreen.js";
+import React from "react";
 import RegistrationScreen from "./Screen/REgistrationScreen.js";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    "Roboto-Regular": require("./assets/Roboto-Regular.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
   });
-  const [isRegister, setIsRegister] = useState(false);
+  // const [isRegister, setIsRegister] = useState(false);
 
   if (!fontsLoaded) {
     return null;
   }
 
-  return <>{!isRegister ? <RegistrationScreen /> : <LoginScreen />}</>;
+  // return <>{!isRegister ? <RegistrationScreen /> : <LoginScreen />}</>;
+  return (
+    <RegistrationScreen />
+    /* <LoginScreen /> */
+  );
 }
