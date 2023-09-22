dd04fd833e73649b34b20d1d79223ffb020f5f01
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-09-22 22:51:59 +0300

    Added KeyboardAvoiding.js. Updated RegistrationScreem.js. Fixed package.json

--- Screen/RegistrationScreen.js
+++ Screen/RegistrationScreen.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import {
-    Image,
-    ImageBackground,
-    Keyboard,
-    KeyboardAvoidingView,
-    Platform,
-    Pressable,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    TouchableWithoutFeedback,
-    View,
-} from "react-native";
-import backgroundImg from "../assets/ScreenBG.png";
-import SvgAddButton from "../assets/svg/svgAddButton";
+  Image,
+  ImageBackground,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import backgroundImg from '../assets/ScreenBG.png';
+import SvgAddButton from '../assets/svg/svgAddButton';
 
 const initialState = {
-    login: '',
-    email: '',
-    password: '',
-    isPasswordFocus: false,
+  login: '',
+  email: '',
+  password: '',
+  isPasswordFocus: false,
 };
 
 export default function RegistrationScreen() {
@@ -28,26 +28,26 @@ export default function RegistrationScreen() {
   const [isAvatar, setAvatar] = useState(false);
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const [isFocused, setIsFocused] = useState({ 1: false, 2: false, 3: false });
+  const [isFocused, setIsFocused] = useState({1: false, 2: false, 3: false});
 
-  const loginHandler = (value) => {
-    setState((prevState) => ({ ...prevState, login: value }));
+  const loginHandler = value => {
+    setState(prevState => ({...prevState, login: value}));
   };
 
-  const emailHandler = (value) => {
-    setState((prevState) => ({ ...prevState, email: value }));
+  const emailHandler = value => {
+    setState(prevState => ({...prevState, email: value}));
   };
 
-  const passwordHandler = (value) => {
-    setState((prevState) => ({ ...prevState, password: value }));
+  const passwordHandler = value => {
+    setState(prevState => ({...prevState, password: value}));
   };
 
   const toggleShowPassword = () => {
-    setShowPassword((prevShowPassword) => !prevShowPassword);
+    setShowPassword(prevShowPassword => !prevShowPassword);
   };
 
-  const setIsPasswordFocus = (value) => {
-    setState((prevState) => ({ ...prevState, isPasswordFocus: value }));
+  const setIsPasswordFocus = value => {
+    setState(prevState => ({...prevState, isPasswordFocus: value}));
   };
 
   const handleSubmit = () => {
@@ -77,17 +77,15 @@ export default function RegistrationScreen() {
         <View style={styles.container}>
           <ImageBackground source={backgroundImg} style={styles.imageBg}>
             <KeyboardAvoidingView
-              behavior={Platform.OS === "ios" ? "padding" : "height"}
-              style={styles.keyboardAvoidingContainer}
-            >
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              style={styles.keyboardAvoidingContainer}>
               <View style={styles.registerSection}>
                 <View style={styles.avatarWrapper}>
-                  <Image source={{ uri: avatar }} style={styles.avatarImg} />
+                  <Image source={{uri: avatar}} style={styles.avatarImg} />
                   <TouchableOpacity
                     style={
                       isAvatar ? styles.btnAddAvatarLoad : styles.btnAddAvatar
-                    }
-                  >
+                    }>
                     <SvgAddButton
                       style={
                         isAvatar
@@ -105,8 +103,8 @@ export default function RegistrationScreen() {
                   placeholder="Логін"
                   placeholderTextColor="#BDBDBD"
                   style={[styles.input, isFocused[1] && styles.isFocused]}
-                  onFocus={() => onFocusHandler({ 1: true })}
-                  onBlur={() => onFocusHandler({ 1: false })}
+                  onFocus={() => onFocusHandler({1: true})}
+                  onBlur={() => onFocusHandler({1: false})}
                 />
                 <TextInput
                   key="email"
@@ -115,53 +113,49 @@ export default function RegistrationScreen() {
                   placeholder="Адреса електронної пошти"
                   style={[styles.input, isFocused[2] && styles.isFocused]}
                   keyboardType="email-address"
-                  onFocus={() => onFocusHandler({ 2: true })}
-                  onBlur={() => onFocusHandler({ 2: false })}
+                  onFocus={() => onFocusHandler({2: true})}
+                  onBlur={() => onFocusHandler({2: false})}
                 />
                 <View
                   key="passwd"
                   style={[
                     styles.passwordContainer,
                     isFocused[3] && styles.isFocused,
-                  ]}
-                >
+                  ]}>
                   <TextInput
                     value={state.password}
                     onChangeText={passwordHandler}
                     placeholder="Пароль"
-                    placeholderTextColor={"#BDBDBD"}
+                    placeholderTextColor={'#BDBDBD'}
                     secureTextEntry={!showPassword}
                     style={styles.passwordInput}
                     onFocus={() => {
                       setIsShowKeyboard(true);
                       setIsPasswordFocus(true);
-                      onFocusHandler({ 3: true });
+                      onFocusHandler({3: true});
                     }}
                     onBlur={() => {
                       setIsPasswordFocus(false);
                       setIsShowKeyboard(false);
-                      onFocusHandler({ 3: false });
+                      onFocusHandler({3: false});
                     }}
                   />
                   <Pressable
                     onPress={toggleShowPassword}
-                    style={styles.toggleButton}
-                  >
+                    style={styles.toggleButton}>
                     <Text style={styles.toggleText}>
-                      {showPassword ? "Сховати" : "Показати"}
+                      {showPassword ? 'Сховати' : 'Показати'}
                     </Text>
                   </Pressable>
-                  <View style={{ width: "100%" }}>
+                  <View style={{width: '100%'}}>
                     <TouchableOpacity
                       onPress={handleSignUp}
-                      style={styles.formBtn}
-                    >
+                      style={styles.formBtn}>
                       <Text style={styles.formBtnTitle}>Зареєструватися</Text>
                     </TouchableOpacity>
 
                     <Pressable
-                      onPress={() => navigation.navigate("LoginScreen")}
-                    >
+                      onPress={() => navigation.navigate('LoginScreen')}>
                       <Text style={styles.linkToLogin}>
                         Вже є акаунт? Увійти
                       </Text>
@@ -180,136 +174,136 @@ export default function RegistrationScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     paddingBottom: 30,
   },
 
   imageBg: {
     flex: 1,
     top: 0,
-    height: "100%",
-    width: "100%",
-    resizeMode: "cover",
-    justifyContent: "flex-end",
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
   },
 
   registerSection: {
-    position: "relative",
-    width: "100%",
+    position: 'relative',
+    width: '100%',
     paddingHorizontal: 16,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
   },
 
   avatarWrapper: {
-    position: "absolute",
+    position: 'absolute',
     top: -60,
     width: 120,
     height: 120,
     borderRadius: 16,
-    backgroundColor: "#F6F6F6",
+    backgroundColor: '#F6F6F6',
   },
   avatarImg: {
     flex: 1,
-    justifyContent: "flex-end",
-    resizeMode: "cover",
+    justifyContent: 'flex-end',
+    resizeMode: 'cover',
   },
   btnAddAvatar: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 14,
     right: -12.5,
 
-    alignItems: "center",
-    alignContent: "center",
+    alignItems: 'center',
+    alignContent: 'center',
 
     width: 25,
     height: 25,
 
-    color: "#ff6c00",
-    backgroundColor: "#ffffff",
+    color: '#ff6c00',
+    backgroundColor: '#ffffff',
     borderRadius: 50,
   },
 
   btnAddAvatarLoad: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 14,
     right: -12.5,
 
-    alignItems: "center",
-    alignContent: "center",
+    alignItems: 'center',
+    alignContent: 'center',
 
     width: 25,
     height: 25,
 
-    color: "#ff6c00",
-    backgroundColor: "#ffffff",
+    color: '#ff6c00',
+    backgroundColor: '#ffffff',
     borderRadius: 50,
 
-    transform: [{ rotate: "45deg" }],
+    transform: [{rotate: '45deg'}],
   },
   btnAddAvatarSvg: {
-    fill: "#ff6c00",
-    stroke: "#ff6c00",
-    backgroundColor: "#ffffff",
+    fill: '#ff6c00',
+    stroke: '#ff6c00',
+    backgroundColor: '#ffffff',
   },
   btnAddAvatarSvgLoad: {
-    fill: "#bdbdbd",
-    stroke: "#e8e8e8",
-    backgroundColor: "#ffffff",
+    fill: '#bdbdbd',
+    stroke: '#e8e8e8',
+    backgroundColor: '#ffffff',
   },
 
   title: {
     marginTop: 92,
     marginBottom: 33,
-    fontFamily: "Roboto-Regular",
+    fontFamily: 'Roboto-Regular',
     fontSize: 30,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     lineHeight: 35.16,
     letterSpacing: 1,
-    textAlign: "center",
-    color: "rgba(33, 33, 33, 1)",
+    textAlign: 'center',
+    color: 'rgba(33, 33, 33, 1)',
   },
   form: {
-    width: "100%",
+    width: '100%',
   },
   input: {
-    width: "100%",
+    width: '100%',
     padding: 16,
     height: 50,
     borderWidth: 1,
     borderRadius: 10,
-    borderColor: "rgba(232, 232, 232, 1)",
-    backgroundColor: "rgba(246, 246, 246, 1)",
+    borderColor: 'rgba(232, 232, 232, 1)',
+    backgroundColor: 'rgba(246, 246, 246, 1)',
     marginBottom: 16,
   },
 
   isFocused: {
-    borderColor: "1px solid rgba(255, 108, 0, 1)",
-    backgroundColor: "#ffffff",
+    borderColor: '1px solid rgba(255, 108, 0, 1)',
+    backgroundColor: '#ffffff',
   },
 
   passwordContainer: {
     height: 50,
     borderWidth: 1,
     borderRadius: 5,
-    borderColor: "rgba(232, 232, 232, 1)",
-    backgroundColor: "rgba(246, 246, 246, 1)",
+    borderColor: 'rgba(232, 232, 232, 1)',
+    backgroundColor: 'rgba(246, 246, 246, 1)',
     marginBottom: 16,
     paddingLeft: 16,
     paddingRight: 16,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 
   passwordInput: {
     flex: 1,
     height: 50,
     fontSize: 16,
-    fontFamily: "Roboto-Regular",
-    fontWeight: "normal",
+    fontFamily: 'Roboto-Regular',
+    fontWeight: 'normal',
     lineHeight: 18.75,
   },
 
@@ -318,49 +312,49 @@ const styles = StyleSheet.create({
   },
 
   toggleText: {
-    textAlign: "center",
+    textAlign: 'center',
     paddingHorizontal: 8,
 
-    fontFamily: "Roboto-Regular",
+    fontFamily: 'Roboto-Regular',
     fontSize: 16,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     lineHeight: 18.75,
 
-    color: "rgba(27, 67, 113, 1)",
+    color: 'rgba(27, 67, 113, 1)',
   },
 
   buttonRg: {
-    fontFamily: "Roboto-Regular",
+    fontFamily: 'Roboto-Regular',
     fontSize: 16,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     lineHeight: 18.75,
-    textAlign: "center",
-    color: "rgba(255, 255, 255, 1)",
+    textAlign: 'center',
+    color: 'rgba(255, 255, 255, 1)',
 
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     height: 51,
     padding: 16,
     borderRadius: 50,
     marginBottom: 15,
-    backgroundColor: "rgba(255, 108, 0, 1)",
+    backgroundColor: 'rgba(255, 108, 0, 1)',
   },
 
   buttonText: {
-    color: "rgba(255, 255, 255, 1)",
+    color: 'rgba(255, 255, 255, 1)',
     fontSize: 16,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     lineHeight: 18.75,
-    fontFamily: "Roboto-Regular",
+    fontFamily: 'Roboto-Regular',
   },
 
   textQ: {
-    fontFamily: "Roboto-Regular",
+    fontFamily: 'Roboto-Regular',
     fontSize: 16,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     lineHeight: 18.75,
-    textAlign: "center",
-    color: "rgba(27, 67, 113, 1)",
+    textAlign: 'center',
+    color: 'rgba(27, 67, 113, 1)',
   },
 });
 exports.default = (0, react_navigation_1.withNavigation)(RegistrationScreen);
