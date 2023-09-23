c462448b874bce5fe491bb01ca7b32f9bc8c3976
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-09-23 17:21:01 +0300

    Configured gradle in the android folder

--- app.config.js
+++ app.config.js
@@ -1,7 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', {value: true});
 const config = {
-    name: "my-app",
-    slug: "my-app",
+  name: 'my-app',
+  slug: 'my-app',
+  android: {
+    package: 'com.anngel.myapp',
+  },
 };
 exports.default = config;
